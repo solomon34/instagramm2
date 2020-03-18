a421000087cf7886e07cf8434eb2f2c1065d6383
Solomon Glover <solomon.glover@hgs.hiddengeniusproject.org>
2020-03-18 15:41:39 -0700

    Form partial and pic show page Bugs

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,17 +1,32 @@
-class PicsController < ApplicationController
+class PicsController < ApplicationController 
+	before_action :find_pic, only: [:show, :edit, :update, :destroy]
 	def index
-	end 
+	end  
+
+	def show 
+		
+	end
 
 	def new 
 		@pic = Pic.new
 	end 
 
 	def create  
-		@pic = Pic.new(pic_params)
+		@pic = Pic.new(pic_params) 
+
+		if @pic.save 
+			redirect_to @pic, notice: "Yes, It was Posted Successfully!" 
+		else 
+			render 'new' 
+		end
 	end 
 
 	private 
 	 def pic_params
 	 	params.require(:pic).permit(:title, :desription)
+	 end 
+
+	 def find_pic 
+	 	@pic = Pic.find(params[:id]) 
 	 end
 end
